fe4939765b6ade2cf86a3b2358df13952c287f4f
Akshadachobhee <akshadachobhe7@gmail.com>
2023-06-24 11:30:40 +0530

    Chnages into project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,9 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
 
-  constructor(private router:Router){}
+  constructor(
+    public router:Router
+    ){}
+  
   title = 'MEAN-Blog';
 }
